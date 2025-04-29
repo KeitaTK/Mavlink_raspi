ce10e6b6834011bdcdc0d25012591886c4b34370
KeitaTK <keita17101042@gmail.com>
2025-04-29 16:29:27 +0900

    attitude

--- pymavlink1.py
+++ pymavlink1.py
@@ -1,11 +1,14 @@
+# mavkinkの情報を受信し、コンソールに表示する。
 from pymavlink import mavutil
+import math
 
-# USB接続の場合、/dev/ttyACM0を指定
-connection = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
-connection.wait_heartbeat()
-print("Heartbeat received")
+conn = mavutil.mavlink_connection('udpin:localhost:14550')
+hb = conn.wait_heartbeat()
+print(f"System ID: {hb.get_srcSystem()}, Component ID: {hb.get_srcComponent()}")
+print(f"Type: {mavutil.mavlink.enums['MAV_TYPE'][hb.type].name}")
+print(f"Autopilot: {mavutil.mavlink.enums['MAV_AUTOPILOT'][hb.autopilot].name}")
 
 while True:
-    msg = connection.recv_match(blocking=True)
-    if msg:
-        print(msg)
+    # 姿勢情報（ATTITUDE）
+    msg = conn.recv_match(type='ATTITUDE', blocking=True)
+    print(f"Roll: {math.degrees(msg.roll):.2f}°, Pitch: {math.degrees(msg.pitch):.2f}°")
