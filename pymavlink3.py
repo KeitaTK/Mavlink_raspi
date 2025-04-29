5d664f0b3ddf3325daef613b64d3338fe0ac97a5
KeitaTK <keita17101042@gmail.com>
2025-04-29 16:39:12 +0900

    hennkou

--- pymavlink3.py
+++ pymavlink3.py
@@ -1,21 +1,8 @@
 from pymavlink import mavutil
-import math  # 追加
 
-# USB接続の場合、/dev/ttyACM0を指定
 connection = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
 connection.wait_heartbeat()
 print("Heartbeat received")
-
-# 姿勢データの送信間隔設定（10Hz）
-connection.mav.command_long_send(
-    connection.target_system,
-    connection.target_component,
-    mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
-    0,
-    mavutil.mavlink.MAVLINK_MSG_ID_ATTITUDE,
-    100000,  # 100000μs = 0.1秒間隔
-    0, 0, 0, 0, 0
-)
 
 while True:
     msg = connection.recv_match(type='ATTITUDE_QUATERNION', blocking=True)
@@ -26,3 +13,4 @@ while True:
         y = msg.q3
         z = msg.q4
         print(f"Quaternion: w={w}, x={x}, y={y}, z={z}")
+
