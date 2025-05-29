9cbdc233ccbc6253a6673032c4a68b15372cb604
KeitaTK <keita17101042@gmail.com>
2025-05-29 22:06:20 +0900

    単純化

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -2974,67 +2974,16 @@ void GCS_MAVLINK::send_autopilot_version() const
  */
 void GCS_MAVLINK::send_taki_custome1() const
 {
-    uint32_t flight_sw_version;
-    uint32_t middleware_sw_version = 0;
-#ifdef APJ_BOARD_ID
-    uint32_t board_version { uint32_t(APJ_BOARD_ID) << 16 };
-#else
-    uint32_t board_version = 0;
-#endif
-    char flight_custom_version[MAVLINK_MSG_AUTOPILOT_VERSION_FIELD_FLIGHT_CUSTOM_VERSION_LEN]{};
-    char middleware_custom_version[MAVLINK_MSG_AUTOPILOT_VERSION_FIELD_MIDDLEWARE_CUSTOM_VERSION_LEN]{};
-    char os_custom_version[MAVLINK_MSG_AUTOPILOT_VERSION_FIELD_OS_CUSTOM_VERSION_LEN]{};
-#ifdef HAL_USB_VENDOR_ID
-    const uint16_t vendor_id { HAL_USB_VENDOR_ID };
-    const uint16_t product_id { HAL_USB_PRODUCT_ID };
-#else
-    uint16_t vendor_id = 0;
-    uint16_t product_id = 0;
-#endif
-    // uint64_t uid = 100;
-    uint64_t uid_taki = 200;
-    uint8_t  uid2[MAVLINK_MSG_AUTOPILOT_VERSION_FIELD_UID2_LEN] = {0};
-
-    uint8_t uid_len = sizeof(uid2); // taken as reference and modified
-                                    // by following call:
-    hal.util->get_system_id_unformatted(uid2, uid_len);
-
-    const AP_FWVersion &version = AP::fwversion();
-
-    flight_sw_version = version.major << (8 * 3) | \
-                        version.minor << (8 * 2) | \
-                        version.patch << (8 * 1) | \
-                        (uint32_t)(version.fw_type) << (8 * 0);
-
-    if (version.fw_hash_str) {
-        strncpy_noterm(flight_custom_version, version.fw_hash_str, ARRAY_SIZE(flight_custom_version));
-    }
-
-    if (version.middleware_hash_str) {
-        strncpy_noterm(middleware_custom_version, version.middleware_hash_str, ARRAY_SIZE(middleware_custom_version));
-    }
-
-    if (version.os_hash_str) {
-        strncpy_noterm(os_custom_version, version.os_hash_str, ARRAY_SIZE(os_custom_version));
-    }
-
+    static uint32_t test_counter = 0;
+    test_counter++;
+    
     mavlink_msg_taki_custome1_send(
         chan,
-        capabilities(),
-        flight_sw_version,
-        middleware_sw_version,
-        version.os_sw_version,
-        board_version,
-        (uint8_t *)flight_custom_version,
-        (uint8_t *)middleware_custom_version,
-        (uint8_t *)os_custom_version,
-        vendor_id,
-        product_id,
-        uid_taki,
-        uid2
+        test_counter,                    // test_counter
+        mavlink_system.sysid,           // taki_system_id
+        mavlink_system.compid           // taki_component_id
     );
 }
-
 
 #if AP_AHRS_ENABLED
 /*
