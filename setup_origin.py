9e2cbf5a4e96688bc02d33cee3ee927aa952e9cd
{KeitaTK} <{keita17101042@gmail.com}>
2025-06-03 18:49:18 +0900

    2

--- setup_origin.py
+++ setup_origin.py
@@ -1,5 +1,6 @@
 from pymavlink import mavutil
-master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
+# master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
+master = mavutil.mavlink_connection('/dev/ttyAMA0', baud=115200)
 master.wait_heartbeat()
 master.mav.set_gps_global_origin_send(
     master.target_system,
