c3618f3061bffe5a8f427fd6fea732263914427c
{KeitaTK} <{keita17101042@gmail.com}>
2025-06-03 18:47:29 +0900

    setup origin

---
+++ setup_origin.py
@@ -0,0 +1,9 @@
+from pymavlink import mavutil
+master = mavutil.mavlink_connection('/dev/ttyACM0', baud=115200)
+master.wait_heartbeat()
+master.mav.set_gps_global_origin_send(
+    master.target_system,
+    int(35.000000 * 1e7),  # 緯度
+    int(135.000000 * 1e7), # 経度
+    0                      # 高度（mm単位、0でOK）
+)
