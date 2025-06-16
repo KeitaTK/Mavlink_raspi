09224c153d9e4f78d86cd214cac2496f4756a73e
KeitaTK <keita17101042@gmail.com>
2025-06-16 18:36:50 +0900

    ６

--- check_Frow.py
+++ check_Frow.py
@@ -1,7 +1,7 @@
 from pymavlink import mavutil
 import time
 print("Pixhawk再起動後、921600bpsで接続テスト")
-time.sleep(5)  # 再起動待ち
+# time.sleep(5)  # 再起動待ち
 
 master = mavutil.mavlink_connection('/dev/ttyAMA0', baud=921600, rtscts=True)
 master.wait_heartbeat()
