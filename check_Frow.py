abd08e1d335fbe0e74c1f19559fd41f3f34b5964
KeitaTK <keita17101042@gmail.com>
2025-06-16 18:35:24 +0900

    ４

--- check_Frow.py
+++ check_Frow.py
@@ -1,17 +1,7 @@
-from pymavlink import mavutil
+import time
+print("Pixhawk再起動後、921600bpsで接続テスト")
+time.sleep(5)  # 再起動待ち
 
-print("115200bpsで接続してパラメータ変更")
-master = mavutil.mavlink_connection('/dev/ttyAMA0', baud=115200, rtscts=True)
+master = mavutil.mavlink_connection('/dev/ttyAMA0', baud=921600, rtscts=True)
 master.wait_heartbeat()
-
-# SERIAL1_BAUDを921600に設定
-master.mav.param_set_send(
-    master.target_system,
-    master.target_component,
-    b'SERIAL1_BAUD',
-    921600,
-    mavutil.mavlink.MAV_PARAM_TYPE_REAL32
-)
-
-print("パラメータ設定完了。Pixhawkを再起動してください。")
-master.close()
+print("921600bps接続成功！")
