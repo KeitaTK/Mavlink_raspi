3566826fb7a069f7f178eac0a8197cba4bc8d886
{KeitaTK} <{keita17101042@gmail.com}>
2025-06-23 21:08:03 +0900

    V

--- No2/check_VERSION.py
+++ No2/check_VERSION.py
@@ -6,7 +6,7 @@ from pymavlink import mavutil
 # telem1で接続するとき
 print("tekem1通信接続")
 # master = mavutil.mavlink_connection('/dev/ttyAMA0', baud=115200)
-master = mavutil.mavlink_connection('/dev/ttyAMA0', baud=921600, rtscts=True)
+master = mavutil.mavlink_connection('/dev/ttyAMA0', 921600, rtscts=True)
 
 # 2. heartbeat 受信待ち
 #    これにより target_system, target_component が設定される
