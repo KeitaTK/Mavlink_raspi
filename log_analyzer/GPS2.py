a77a3349b280fe17c76bab0275dd298fd07fc21d
KeitaTK <keita17101042@gmail.com>
2025-06-26 13:16:01 +0900

    確認

---
+++ log_analyzer/GPS2.py
@@ -0,0 +1,18 @@
+from pymavlink import mavutil
+import os
+
+log_file = '/home/taki/Mavlink_raspi/log_analyzer/bin/00000058.BIN'
+
+if not os.path.exists(log_file):
+    print(f"エラー: ログファイル {log_file} が見つかりません。パスを確認してください。")
+    exit(1)
+
+mlog = mavutil.mavlink_connection(log_file)
+msg_types = set()
+while True:
+    msg = mlog.recv_match(blocking=False)
+    if msg is None:
+        break
+    msg_types.add(msg.get_type())
+
+print("ログファイルに含まれるメッセージタイプ:", sorted(msg_types))
