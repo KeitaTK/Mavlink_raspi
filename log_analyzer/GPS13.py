85de5aa621e72814582390228ebaa0d42559b96a
{KeitaTK} <{keita17101042@gmail.com}>
2025-06-28 19:28:02 +0900

    107

--- log_analyzer/GPS13.py
+++ log_analyzer/GPS13.py
@@ -5,8 +5,8 @@ import numpy as np
 from pyproj import Transformer
 
 # ログファイルのパス
-log_file = '/home/taki/Mavlink_raspi/log_analyzer/LOGS1/00000105.BIN'
-output_csv = '/home/taki/Mavlink_raspi/log_analyzer/CSV/local_11.csv'
+log_file = '/home/taki/Mavlink_raspi/log_analyzer/LOGS1/00000107.BIN'
+output_csv = '/home/taki/Mavlink_raspi/log_analyzer/CSV/local_12.csv'
 
 # 基準座標（ローカル座標系の原点）
 ref_lat = 36.0757800
