1efc41e7708bd40c05a33f5ad3bf6a98dd2de32b
{KeitaTK} <{keita17101042@gmail.com}>
2025-06-29 17:30:02 +0900

    pid

--- No1/setup_all_tuning.py
+++ No1/setup_all_tuning.py
@@ -96,12 +96,12 @@ params_to_set = {
     'PSC_VELXY_D': 0.5,      # 水平速度制御D
     
     # --- 姿勢制御PID ---
-    'ATC_RAT_RLL_P': 0.04,  # Roll P
+    'ATC_RAT_RLL_P': 0.03,  # Roll P
     'ATC_RAT_RLL_I': 0.07,  # Roll I
-    'ATC_RAT_RLL_D': 0.0008, # Roll D
-    'ATC_RAT_PIT_P': 0.05,  # Pitch P
+    'ATC_RAT_RLL_D': 0.001, # Roll D
+    'ATC_RAT_PIT_P': 0.04,  # Pitch P
     'ATC_RAT_PIT_I': 0.07,  # Pitch I
-    'ATC_RAT_PIT_D': 0.0008, # Pitch D
+    'ATC_RAT_PIT_D': 0.001, # Pitch D
     'ATC_RAT_YAW_P': 0.2,    # Yaw P
     'ATC_RAT_YAW_I': 0.02,   # Yaw I
     
