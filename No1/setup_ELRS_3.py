b693a00dfc63f109b6e23068b3ac1c0b1867e327
KeitaTK <keita17101042@gmail.com>
2025-09-26 18:25:15 +0900

    姿勢制御PIDのRollおよびPitchのPゲインを1.2倍から1.1倍に、Dゲインを1.4倍から1.2倍に変更

--- No1/setup_ELRS_3.py
+++ No1/setup_ELRS_3.py
@@ -102,12 +102,12 @@ params_to_set = {
     'PSC_VELXY_D': 0.3,      # 水平速度制御D  増やせるかも
     
     # --- 姿勢制御PID ---
-    'ATC_RAT_RLL_P': 0.04*1.2,  # Roll P
+    'ATC_RAT_RLL_P': 0.04*1.1,  # Roll P
     'ATC_RAT_RLL_I': 0.05,  # Roll I
-    'ATC_RAT_RLL_D': 0.0012*1.4, # Roll D
-    'ATC_RAT_PIT_P': 0.05*1.2,  # Pitch P
+    'ATC_RAT_RLL_D': 0.0012*1.2, # Roll D
+    'ATC_RAT_PIT_P': 0.05*1.1,  # Pitch P
     'ATC_RAT_PIT_I': 0.05,  # Pitch I
-    'ATC_RAT_PIT_D': 0.0012*1.4, # Pitch D
+    'ATC_RAT_PIT_D': 0.0012*1.2, # Pitch D
     'ATC_RAT_YAW_P': 0.2,    # Yaw P
     'ATC_RAT_YAW_I': 0.02,   # Yaw I
 
