f37b8204163b881cdebd52090fb8af38d16289f4
KeitaTK <keita17101042@gmail.com>
2026-05-11 19:12:04 +0900

    機体振動を止めるように姿勢制御のゲインを下げる

--- EKF/setup_EKF_Observer.py
+++ EKF/setup_EKF_Observer.py
@@ -120,10 +120,10 @@ params_to_set = {
     'PSC_ACCZ_I': (1.0, 'AP_Float', '垂直加速度制御I'),
     
     # === 水平制御PID ===
-    'PSC_POSXY_P': (5.0, 'AP_Float', '水平位置制御P'),
+    'PSC_POSXY_P': (2.0, 'AP_Float', '水平位置制御P'),
     'PSC_VELXY_P': (4.0, 'AP_Float', '水平速度制御P'),
     'PSC_VELXY_I': (2.5, 'AP_Float', '水平速度制御I'),
-    'PSC_VELXY_D': (1.2, 'AP_Float', '水平速度制御D'),
+    'PSC_VELXY_D': (0.6, 'AP_Float', '水平速度制御D'),
     
     # === 姿勢制御PID ===
     'ATC_RAT_RLL_P': (0.04, 'AP_Float', 'Roll P'),
